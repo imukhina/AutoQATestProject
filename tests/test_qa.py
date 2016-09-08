af5642c8b309a5937541577f4fedf0229a272ad4
sinnermor <kordyukova.a@mail.ru>
2016-09-08 12:20:22 +0300

    fix fixture problems

--- tests/test_qa.py
+++ tests/test_qa.py
@@ -1,17 +1,11 @@
 #-*- coding: utf-8 -*-
 
-import pytest
-from application import Application
 import socket
 
 
 s = socket.socket()
 s.connect(("irr.ru", 80))
 
-@pytest.fixture(scope="module")
-def app():
-
-    return Application()
 
 
 def test_search_in_python_org(app):
